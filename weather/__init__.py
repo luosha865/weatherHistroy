9d8f1c68811424dfd28e710c40bcb79834a92581
luosha865 <luosha865@gmail.com>
2014-08-07 23:34:40 +0800

    init

---
+++ weather/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'luosha865'
---
+++ weather/crawler.py
@@ -0,0 +1,9 @@
+__author__ = 'luosha865'
+import urllib2
+
+class Crawler:
+    def __int__(self):
+        pass
+    def ReadPage(self,url):
+        page = urllib2.urlopen(url)
+    
